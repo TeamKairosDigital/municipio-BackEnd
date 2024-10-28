fd48828280d00cd660792edca82bdc3268afa7b3
luisrodriguez0698 <luisrodriguez0698@gmail.com>
2024-10-28 11:50:45 -0600

    conectando api obras

--- src/controllers/obras.controller.ts
+++ src/controllers/obras.controller.ts
@@ -1,4 +1,5 @@
-import { Body, Controller, Delete, Get, Param, Post, Put } from '@nestjs/common';
+import { Body, Controller, Delete, Get, Param, Post, Put, UploadedFile } from '@nestjs/common';
+import { ApiResponse } from 'src/models/ApiResponse';
 import { CreateObrasDto } from 'src/models/dto/obrasDto';
 import { Obras } from 'src/models/obras.entity';
 import { ObrasService } from 'src/services/obras.service';
@@ -8,29 +9,40 @@ export class ObrasController {
 
     constructor(private readonly obrasService: ObrasService) {}
 
-    @Post()
-    create(@Body() createObrasDto: CreateObrasDto): Promise<Obras> {
-      return this.obrasService.create(createObrasDto);
-    }
-  
-    @Get()
-    findAll(): Promise<Obras[]> {
-      return this.obrasService.findAll();
-    }
-  
-    @Get(':id')
-    findOne(@Param('id') id: number): Promise<Obras> {
-      return this.obrasService.findOne(id);
-    }
-  
-    @Put(':id')
-    update(@Param('id') id: number, @Body() updateObrasDto: CreateObrasDto): Promise<Obras> {
-      return this.obrasService.update(id, updateObrasDto);
-    }
-  
-    @Delete(':id')
-    remove(@Param('id') id: number): Promise<void> {
-      return this.obrasService.remove(id);
-    }
+  // Crear nueva obra
+  @Post()
+  create(
+    @Body() createObrasDto: CreateObrasDto,
+    @UploadedFile() file: Express.Multer.File
+  ): Promise<ApiResponse<any>> {
+    return this.obrasService.create(createObrasDto, file);
+  }
+
+  // Obtener lista de obras
+  @Get()
+  findAll(): Promise<ApiResponse<any>> {
+    return this.obrasService.findAll();
+  }
+
+  // Obtener un obra por ID
+  @Get(':id')
+  findOne(@Param('id') id: number): Promise<ApiResponse<any>> {
+    return this.obrasService.findOne(id);
+  }
+
+  // Actualizar una obra
+  @Put(':id')
+  update(@Param('id') id: number, 
+  @Body() updateObrasDto: CreateObrasDto,  
+  @UploadedFile() file: Express.Multer.File
+): Promise<ApiResponse<any>> {
+    return this.obrasService.update(id, updateObrasDto);
+  }
+
+  // Eliminar un obra
+  @Delete(':id')
+  remove(@Param('id') id: number): Promise<ApiResponse<any>> {
+    return this.obrasService.remove(id);
+  }
 
 }
--- src/services/obras.service.ts
+++ src/services/obras.service.ts
@@ -1,77 +1,187 @@
 import { Injectable, InternalServerErrorException, NotFoundException } from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
+import { ApiResponse } from 'src/models/ApiResponse';
 import { CreateObrasDto } from 'src/models/dto/obrasDto';
 import { Obras } from 'src/models/obras.entity';
 import { Repository } from 'typeorm';
+import { S3Service } from './s3.service';
 
 @Injectable()
 export class ObrasService {
 
     constructor(
         @InjectRepository(Obras)
-        private readonly blogRepository: Repository<Obras>,
+        private readonly obrasRepository: Repository<Obras>,
+        private readonly s3: S3Service
       ) {}
 
-  // Crear un nuevo blog
-  async create(createObrasDto: CreateObrasDto): Promise<Obras> {
+  // Crear nueva obra
+  async create(createObrasDto: CreateObrasDto, file: Express.Multer.File): Promise<ApiResponse<Obras>> {
+
     try {
-      const newBlog = this.blogRepository.create(createObrasDto);
-      return await this.blogRepository.save(newBlog);
+
+      const newObra = this.obrasRepository.create(createObrasDto);
+      const savedObra = await this.obrasRepository.save(newObra);
+
+      return {
+        success: true,
+        statusCode: 201,
+        message: 'Obra creado con éxito',
+        data: savedObra,
+      };
+
     } catch (error) {
-      throw new InternalServerErrorException('Error al crear el blog');
+
+      return {
+        success: false,
+        statusCode: 500,
+        message: 'Error al crear la obra',
+        errors: error.message,
+      };
+
     }
+
   }
 
-  // Obtener todos los blogs
-  async findAll(): Promise<Obras[]> {
+  // Obtener lista de obras
+  async findAll(): Promise<ApiResponse<Obras[]>> {
+
     try {
-      return await this.blogRepository.find();
+
+      const Obras = await this.obrasRepository.find();
+
+      return {
+        success: true,
+        statusCode: 200,
+        message: 'Obras obtenidos con éxito',
+        data: Obras,
+      };
+
     } catch (error) {
-      throw new InternalServerErrorException('Error al obtener los blogs');
+
+      return {
+        success: false,
+        statusCode: 500,
+        message: 'Error al obtener las obras',
+        errors: error.message,
+      };
+
     }
+
   }
 
-  // Obtener un blog por ID
-  async findOne(id: number): Promise<Obras> {
+  // Obtener un obra por ID
+  async findOne(id: number): Promise<ApiResponse<Obras>> {
+
     try {
-      const blog = await this.blogRepository.findOneBy({ id });
-      if (!blog) {
-        throw new NotFoundException(`Blog con ID ${id} no encontrado`);
+
+      const Obra = await this.obrasRepository.findOneBy({ id });
+      
+      if (!Obra) {
+        return {
+          success: false,
+          statusCode: 404,
+          message: `Obra con ID ${id} no encontrado`,
+        };
       }
-      return blog;
+
+      return {
+        success: true,
+        statusCode: 200,
+        message: 'Obra obtenido con éxito',
+        data: Obra,
+      };
+
     } catch (error) {
-      throw new InternalServerErrorException('Error al obtener el blog');
+
+      return {
+        success: false,
+        statusCode: 500,
+        message: 'Error al obtener la Obra',
+        errors: error.message,
+      };
+
     }
+
   }
 
-  // Actualizar un blog
-  async update(id: number, updateObrasDto: CreateObrasDto): Promise<Obras> {
+  // Actualizar una obra
+  async update(id: number, updateObrasDto: CreateObrasDto): Promise<ApiResponse<Obras>> {
+
     try {
-      const blog = await this.blogRepository.preload({
+
+      const Obra = await this.obrasRepository.preload({
         id,
         ...updateObrasDto,
       });
 
-      if (!blog) {
-        throw new NotFoundException(`Blog con ID ${id} no encontrado`);
+      if (!Obra) {
+
+        return {
+          success: false,
+          statusCode: 404,
+          message: `Obra con ID ${id} no encontrado`,
+        };
+
       }
 
-      return await this.blogRepository.save(blog);
+      const updatedObra = await this.obrasRepository.save(Obra);
+
+      return {
+        success: true,
+        statusCode: 200,
+        message: 'Obra actualizado con éxito',
+        data: updatedObra,
+      };
+
     } catch (error) {
-      throw new InternalServerErrorException('Error al actualizar el blog');
+
+      return {
+        success: false,
+        statusCode: 500,
+        message: 'Error al actualizar el Obra',
+        errors: error.message,
+      };
+
     }
+
   }
 
-  // Eliminar un blog
-  async remove(id: number): Promise<void> {
+  // Eliminar un obra
+  async remove(id: number): Promise<ApiResponse<any>> {
+
     try {
-      const result = await this.blogRepository.delete(id);
+
+      const result = await this.obrasRepository.delete(id);
+
       if (result.affected === 0) {
-        throw new NotFoundException(`Blog con ID ${id} no encontrado`);
+
+        return {
+          success: false,
+          statusCode: 404,
+          message: `Obra con ID ${id} no encontrado`,
+        };
+
       }
+
+      return {
+        success: true,
+        statusCode: 200,
+        message: 'Obra eliminado con éxito',
+        data: null,
+      };
+
     } catch (error) {
-      throw new InternalServerErrorException('Error al eliminar el blog');
+
+      return {
+        success: false,
+        statusCode: 500,
+        message: 'Error al eliminar el Obra',
+        errors: error.message,
+      };
+      
     }
+
   }
 
 }
