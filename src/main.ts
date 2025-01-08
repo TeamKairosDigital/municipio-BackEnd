d63226f2795192e8f6466a2a0f3766a4f71855ef
TeamKairosDigital <kairos0698@gmail.com>
2025-01-08 10:51:00 -0600

    agregando cors

--- src/main.ts
+++ src/main.ts
@@ -8,9 +8,20 @@ async function bootstrap() {
 
   const app = await NestFactory.create(AppModule);
 
+  const allowedOrigins = [
+    'http://localhost:4200',
+    'https://larrainzar-front-nhil2.ondigitalocean.app',
+  ];
+
   // Habilitar CORS
   app.enableCors({
-    origin: 'http://localhost:4200', // Origen permitido
+    origin: (origin, callback) => {
+      if (!origin || allowedOrigins.includes(origin)) {
+        callback(null, true);
+      } else {
+        callback(new Error('Not allowed by CORS'));
+      }
+    },
     methods: 'GET,HEAD,PUT,PATCH,POST,DELETE',
     credentials: true,
   });
